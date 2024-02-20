a2929140e560a0ac04a77625bbafe0aaa0974068
Rolo <roloedits@gmail.com>
2024-02-19 22:49:12 -0800

    refactor: simplify the working example to better target issues as they come up

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,13 +1,15 @@
 use calamine::wip::{Workbook, Xlsx};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = r#"NYC_311_SR_2010-2020-sample-1M.xlsx"#;
+    let path = "NYC_311_SR_2010-2020-sample-1M.xlsx";
 
     let mut workbook = Xlsx::open(path)?;
 
-    let worksheet = workbook.worksheet("sheet1")?.unwrap();
+    let worksheet = workbook
+        .worksheet("NYC_311_SR_2010-2020-sample-1M")
+        .unwrap();
 
-    for row in worksheet.rows() {}
+    for _row in worksheet.rows() {}
 
     Ok(())
 }
